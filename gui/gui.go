d57616cc0c513cbc0aa519bf069d47f5b9878417
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2015-06-26 10:35:01 +0200

    add --ip and --port options

--- gui/gui.go
+++ gui/gui.go
@@ -4,9 +4,12 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/GeertJohan/go.rice"
+	"github.com/codegangsta/cli"
 	"github.com/codegangsta/negroni"
 	"github.com/julienschmidt/httprouter"
 	"github.com/nbio/httpcontext"
@@ -22,34 +25,54 @@ var assets http.FileSystem
 
 // LaunchServer launches HTTP server
 func main() {
-	router := httprouter.New()
-	router.HandlerFunc("GET", "/ping", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("pong"))
-	})
+	app := cli.NewApp()
+	app.Name = "pepper gui"
+	app.Usage = "User interface for pepper"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "ip",
+			Value: "127.0.0.1",
+			Usage: "IP to bind",
+		},
+		cli.IntFlag{
+			Name:  "port",
+			Value: 6480,
+			Usage: "port to bind",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		router := httprouter.New()
+		// prod (tag dev is not present)
+		if assets == nil {
+			// rices boxes
+			httpbox := rice.MustFindBox("../assets/http")
+			assets = httpbox.HTTPBox()
+		}
 
-	// prod (tag dev is not present)
-	if assets == nil {
-		// rices boxes
-		httpbox := rice.MustFindBox("../assets/http")
-		assets = httpbox.HTTPBox()
+		// assets
+		router.Handler("GET", "/assets/*res", http.StripPrefix("/assets/", http.FileServer(assets)))
+
+		// index
+		router.HandlerFunc("GET", "/", handlerIndex)
+
+		// http server
+		httpAddr := c.String("ip") + ":" + strconv.FormatInt(int64(c.Int("port")), 10)
+		browserAddr := httpAddr
+		if c.String("ip") == "0.0.0.0" {
+			browserAddr = "127.0.0.1:" + strconv.FormatInt(int64(c.Int("port")), 10)
+		}
+
+		n := negroni.New(negroni.NewRecovery())
+		n.UseHandler(router)
+		select {
+		case <-time.After(1 * time.Second):
+			browser.OpenURL(browserAddr)
+		}
+		log.Println("GUI HTTP server listening on " + httpAddr)
+		log.Fatalln(http.ListenAndServe(httpAddr, n))
 	}
 
-	// assets
-	router.Handler("GET", "/assets/*res", http.StripPrefix("/assets/", http.FileServer(assets)))
-
-	// index
-	router.HandlerFunc("GET", "/", handlerIndex)
-
-	// http server
-	n := negroni.New(negroni.NewRecovery())
-	n.UseHandler(router)
-	select {
-	case <-time.After(1 * time.Second):
-		browser.OpenURL("http://127.0.0.1:6480/")
-	}
-	log.Println("GUI HTTP server will be lanched on http://127.0.0.1:6480")
-	log.Fatalln(http.ListenAndServe("0.0.0.0:6480", n))
-	// launch browser
+	app.Run(os.Args)
 
 }
 
@@ -62,6 +85,7 @@ func wrapHandler(h func(http.ResponseWriter, *http.Request)) httprouter.Handle {
 	}
 }
 
+// handlerIndex serves /
 func handlerIndex(w http.ResponseWriter, req *http.Request) {
 	file, err := assets.Open("index.html")
 	if err != nil {
